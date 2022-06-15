8e4b55641906f71611ca562d58a162727e8a300e
A2L5E0X1 <alexed25@protonmail.com>
2022-06-15 23:10:18 +0200

    huawei: initial commit
    Change-Id: Ia38251923a0603ddc3ca195bd19ecb984667044b

---
+++ Android.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+HARDWARE_PATH := $(call my-dir)
+
+ifeq ($(BOARD_VENDOR),huawei)
+
+endif
---
+++ README.md
@@ -0,0 +1,4 @@
+# Hardware specific parts for Kirin devices
+List of supported chips:
+* Kirin 650 series
+* Kirin 970
